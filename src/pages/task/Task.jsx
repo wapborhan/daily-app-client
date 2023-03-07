f729d1bfaf625ce878940c7a982cf3d7b5b02906
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2023-03-07 16:23:05 +0600

    Directory Changes

--- src/pages/task/Task.jsx
+++ src/pages/task/Task.jsx
@@ -1,44 +1,104 @@
-import React from "react";
-import { Card, CardBody, CardHeader, Table } from "reactstrap";
-const Task = () => {
-  return (
-    <Card
-      style={{
-        width: "100%",
-      }}
-      // className="shadow"
-    >
-      <CardHeader className="h4 fw-bold">Task</CardHeader>
-      <CardBody className="p-0">
-        <Table bordered hover responsive striped>
-          <thead>
-            <tr>
-              <th>SL</th>
-              <th>Date</th>
-              <th>Task</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>01/03/2023</td>
-              <td>Otto</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>01/03/2023</td>
-              <td>Thornton</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>01/03/2023</td>
-              <td>the Bird</td>
-            </tr>
-          </tbody>
-        </Table>
-      </CardBody>
-    </Card>
-  );
-};
+import React, { Fragment, Component } from "react";
+import IncomeList from "./IncomeList";
 
-export default Task;
+export default class Income extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      indescval: "",
+      inammval: "",
+      isSubmitDisabled: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  // triggered everytime value changes in our textboxes
+  handleChange(event) {
+    this.setState(
+      {
+        // use dynamic name value to set our state object property
+        [event.target.name]: event.target.value,
+      },
+      function () {
+        this.canSubmit();
+      }
+    );
+  }
+
+  canSubmit() {
+    const { indescval, inammval } = this.state;
+    // If Input Value is blank Button are Disable
+    if (indescval.length > 0 && inammval.length > 0) {
+      this.setState({
+        isSubmitDisabled: false,
+      });
+    } else {
+      this.setState({
+        isSubmitDisabled: true,
+      });
+    }
+  }
+  // triggered on submit
+  handleSubmit = (event) => {
+    // get our const values by destructuring state
+    event.preventDefault();
+    const { indescval, inammval } = this.state;
+    // regular javascript alert function
+    alert(`Your type detail: \n 
+           Desc: ${indescval} \n 
+           Amount: ${inammval} \n
+           `);
+  };
+  render() {
+    document.title = "Income - SR Book";
+    return (
+      <Fragment>
+        <div className="border shadow rounded mb-3">
+          <div className="form m-3">
+            <form onSubmit={this.handleSubmit}>
+              <div className="row">
+                <div className="col-md-6">
+                  <input
+                    className="mb-2"
+                    id="income-detail"
+                    type="text"
+                    placeholder="আয়ের বিবরণ"
+                    name="indescval"
+                    value={this.state.indescval}
+                    onChange={this.handleChange}
+                    required
+                  />
+                </div>
+                <div className="col-md-3">
+                  <input
+                    className="mb-2"
+                    id="incamount"
+                    type="number"
+                    placeholder="পরিমান"
+                    name="inammval"
+                    value={this.state.inammval}
+                    onChange={this.handleChange}
+                    required
+                  />
+                </div>
+                <div className="col-md-3">
+                  <button
+                    className="btn btn-success"
+                    id="income-btn"
+                    data-bs-toggle="modal"
+                    data-bs-target="#incomemodal"
+                    disabled={this.state.isSubmitDisabled}
+                  >
+                    আপডেট{" "}
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        <IncomeList data={this.props.data} />
+      </Fragment>
+    );
+  }
+}
